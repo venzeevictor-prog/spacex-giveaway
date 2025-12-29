77d5aca5d692d26e5a82c19eb0c935783ca70559
venzeevictor-prog <venzeevictor@gmail.com>
2025-12-29 11:39:29 +0100

    Add files via upload

---
+++ www/reward.js
@@ -0,0 +1,87 @@
+window.onload =async  function () {
+
+    
+   fetch("/track-view", {
+      method: "POST"
+    });
+
+   let refresh;
+
+    fetch(window.location+'/reviews', {
+             method: 'POST',
+                credentials:'include',
+               headers: {
+                 'Content-Type': 'application/json',
+                  
+               },
+               body: JSON.stringify({ipaddress: '192.168.01'}) // Convert the data to a JSON string
+            
+    })
+    .then(async  response => {
+         const data = await response.json()
+         console.log(data)
+         refresh = data.refresh;
+            const arr = data.initial_data_list
+         for (let index = 0; index < arr.length; index++) {
+            
+          let item = arr[index]
+         document.querySelector('.comment').insertAdjacentHTML('beforeend', `
+              <div class="comment-text">
+                <span class="ad"><b>${item.address}:</b></span>
+                <span  class="ad"> ${item.content_source}</span>
+                <img src="${item.Image_src_url}" width="50" height="60" alt="" srcset="">
+            </div>
+            `)
+         }
+
+         
+     let elem_count = 0;
+setInterval(() =>{
+    elem_count ++
+  
+         let display = refresh[elem_count]
+
+         document.querySelector('.comment').insertAdjacentHTML('afterbegin', `
+              <div class="comment-text">
+                <span class="ad"><b>${display.address}:</b></span>
+                <span  class="ad"> ${display.content_source}</span>
+                <img src="${display.Image_src_url}" width="50" height="60" alt="" srcset="">
+            </div>
+            `)
+      
+}, 5000)
+    })
+
+
+
+
+}
+  
+
+let time = 9 * 60; // 9 minutes in seconds
+    const timerElement = document.getElementById("timer");
+
+    function updateTimer() {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+
+        timerElement.textContent = 'Time left ' +
+            String(minutes).padStart(2, "0") + ":" +
+            String(seconds).padStart(2, "0");
+
+            if (time < 4) {
+                document.querySelector('.timer').style.color = 'red';
+            }else if (document.querySelector('.timer').style.textContent == '0:00') {
+                document.querySelector('.timer').style.textContent = 'Time out';
+            }else{
+               document.querySelector('.timer').style.color = 'black';
+            }
+        if (time > 0) {
+            time--;
+        } else {
+            clearInterval(countdown);
+        }
+    }
+
+    updateTimer(); // initial display
+    const countdown = setInterval(updateTimer, 1000);
